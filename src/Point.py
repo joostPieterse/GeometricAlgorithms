711008305363f130550583330b01a64851d5eaf4
Joost Pieterse <joostpieterse@live.nl>
2018-01-09 10:59:48 +0100

    Fixed isRightOf and isAbove

--- src/Point.py
+++ src/Point.py
@@ -8,11 +8,10 @@ class Point:
         return "(%s, %s)" % (self.x, self.y)
 
     def isAbove(self, point1, point2):
-        result = self.y > (self.x - point1[0]) / (point2[0] - point1[0]) * (point2[1] / point1[1]) + point1[1]
+        result = self.y > (self.x - point1[0]) / (point2[0] - point1[0]) * (point2[1] - point1[1]) + point1[1]
         print("isAbove", point1, point2, self, result)
         return result
 
     def isRightOf(self, point1, point2):
-        result = self.x > (self.y - point1[1]) / (point2[1] - point1[1]) * (point2[0] / point1[0]) + point1[0]
-        print("isRightOf", point1, point2, self, result)
+        result = self.x > (self.y - point1[1]) / (point2[1] - point1[1]) * (point2[0] - point1[0]) + point1[0]
         return result
